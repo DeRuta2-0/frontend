a98b92360ce139222d8fb438f8da3c887eb3f5ed
cdondovich <cdondovich@primary.com.ar>
2022-02-08 22:08:58 -0300

    Intengo de pantalla de login

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.text} placeholder={"Usuario"}></TextInput>
+      <TextInput style={styles.text} placeholder={"Contraseña"}></TextInput>
       <StatusBar style="auto" />
+      <Button titleStyle={styles.textButton} title="LOG IN"></Button>
     </View>
   );
 }
@@ -17,4 +20,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 25,
+    marginBottom: 30
+  },
+  textButton: {
+    fontSize: 25
+  }
 });
