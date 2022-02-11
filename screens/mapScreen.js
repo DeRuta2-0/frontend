89044a79ae15ae0ccc3c3aeed49fda59296704b1
cdondovich <cdondovich@primary.com.ar>
2022-02-11 01:17:44 -0300

    Adding first pins

--- screens/mapScreen.js
+++ screens/mapScreen.js
@@ -1,22 +1,36 @@
-import {Dimensions, StyleSheet, View} from "react-native";
+import {Dimensions, StyleSheet, View, Image} from "react-native";
 import MapView, {PROVIDER_GOOGLE} from "react-native-maps";
 import * as React from "react";
 
 export function MapScreen({ navigation }) {
     return (
         <View style={styles.container}>
+
+
             <MapView
                 customMapStyle={mapStyle}
                 provider={PROVIDER_GOOGLE}
                 style={styles.mapStyle}
-                initialRegion={{
-                    latitude: 41.3995345,
-                    longitude: 2.1909796,
-                    latitudeDelta: 0.003,
-                    longitudeDelta: 0.003,
+                loadingEnabled={true}
+                region={{
+                    latitude: -15.59611,
+                    longitude: -56.09667,
+                    latitudeDelta: 70,
+                    longitudeDelta: 1
                 }}
                 mapType="standard"
-            ></MapView>
+            >
+                {response.map(marker => (
+                    <MapView.Marker
+                        key={marker.id}
+                        coordinate={marker.coordinates}
+                        title={marker.title}
+                        description={marker.description}
+                    >
+                        <Image source={ marker.icon } style={{ height: 32, width: 32 }} />
+                    </MapView.Marker>
+                ))}
+            </MapView>
         </View>
     );
 }
@@ -27,6 +41,87 @@ const styles = StyleSheet.create({
         height: Dimensions.get('window').height,
     }
 });
+
+const height = Dimensions.get('window').height;
+
+const response = [
+    {
+        id: '1',
+        coordinates: {
+            latitude: -15.59611,
+            longitude: -56.09667,
+        },
+        title: 'Cuiabá',
+        description: 'Cuiabá',
+        icon: require('../assets/pin1.png')
+    },
+    {
+        id: '2',
+        coordinates: {
+            latitude: -20.44278,
+            longitude: -54.64639,
+        },
+        title: 'Campo Grande',
+        description: 'Campo Grande',
+        category: 1,
+        icon: require('../assets/pin1.png')
+    },
+    {
+        id: '3',
+        coordinates: {
+            latitude: -23.5489,
+            longitude: -46.6388,
+        },
+        title: 'São Paulo',
+        description: 'São Paulo',
+        category: 1,
+        icon: require('../assets/pin2.png')
+    },
+    {
+        id: '4',
+        coordinates: {
+            latitude: -22.9035,
+            longitude: -43.2096,
+        },
+        title: 'Rio de Janeiro',
+        description: 'Rio de Janeiro',
+        category: 1,
+        icon: require('../assets/pin2.png')
+    },
+    {
+        id: '5',
+        coordinates: {
+            latitude: -3.71839,
+            longitude: -38.5434,
+        },
+        title: 'Ceará',
+        description: 'Ceará',
+        category: 1,
+        icon: require('../assets/pin3.png')
+    },
+    {
+        id: '6',
+        coordinates: {
+            latitude: -30.0277,
+            longitude: -51.2287,
+        },
+        title: 'Rio Grande do Sul',
+        description: 'Rio Grande do Sul',
+        category: 1,
+        icon: require('../assets/pin3.png')
+    },
+    {
+        id: '7',
+        coordinates: {
+            latitude: -9.974,
+            longitude: -67.8076,
+        },
+        title: 'Acre',
+        description: 'Acred',
+        category: 1,
+        icon: require('../assets/pin3.png')
+    }
+]
 
 const mapStyle = [
     {
