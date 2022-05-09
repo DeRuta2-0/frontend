08f00be7ae4d9725e0d0b1818b57bdd884851737
cdondovich <cdondovich@primary.com.ar>
2022-05-08 22:09:19 -0300

    deleted localProperties

--- localProperties.js
+++
@@ -1,5 +0,0 @@
-export const usingServer = true;
-export const serverIp = '192.168.0.251';
-
-export const image4ioApiKey = '68S8aaojVb8KTtM5oAp/kA==';
-export const image4ioApiSecret = '87clUOHM3eZ+jYDo7GpSXk+I27ylk8LBsZjAE61CkN0=';
